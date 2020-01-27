3fdc53853df9e0380f41d4e511e464afe02c4ebf
Fuqing Wang <fuqingwang@dhcp-wifi-8021x-155-41-107-175.bu.edu>
2020-01-27 17:21:41 -0500

    lake

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -3,13 +3,11 @@ def draw_lake(size=4):
     for i in range(size):
         print("----", end="")
     print("\\")
-    
     for i in range(size):
         print("|", end="")
         for j in range(size):
             print("~ ~ ", end="")
         print("|")
-        
     print("\\", end="")
     for i in range(size):
         print("----", end="")
