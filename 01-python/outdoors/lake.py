db9f1cbaacb034ffe8bc753a1a244a703803a319
Fuqing Wang <fuqingwang@dhcp-wifi-8021x-155-41-107-175.bu.edu>
2020-01-27 17:11:00 -0500

    resizable lake

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,5 +1,16 @@
-def draw_lake(size=1):
-    print("/-----\\")
-    print("|~ ~ ~|")
-    print("\-----/")
+def draw_lake(size=4):
+    print("/", end="")
+    for i in range(size):
+        print("----", end="")
+    print("\\")
+
+    for i in range(size):
+        print("|", end="")
+        for j in range(size):
+            print("~ ~ ", end="")
+        print("|")
+    print("\\", end="")
+    for i in range(size):
+        print("----", end="")
+    print("/")
     return
